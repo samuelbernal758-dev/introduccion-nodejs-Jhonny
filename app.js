dfd8c8816aa04eb4c48efdc6a6a6be96b0f23625
nacoooobit <johannicolasad@gmail.com>
2026-07-23 15:45:21 -0500

    Primer commit de nodejs

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (__,res)=>{
+    res.send ('Hola, Estamos aprendiendo express con la ficha 3407184');
+});
+
+app.listen(port, ()=>{
+    console.log(`Servidor en funcionamiento en el puerto: `);
+});
+

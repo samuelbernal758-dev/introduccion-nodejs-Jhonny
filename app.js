ec46634add953a52e3f5dc13ae8b6c4074973594
yoberson <yobersond@gmail.com>
2026-07-30 12:45:16 -0500

    Cambiando de puerto en node.js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 import express from 'express';
+
 const app = express();
-const port = 3000;
+const port = 3030;
+
+export const Saludo = "Hola, estamops aprendiendo exress con la ficha 3407184"
 
 app.get("/", (__,res)=>{
-    res.send ('Hola, Estamos aprendiendo express con la ficha 3407184');
+    res.send (Saludo);
 });
 
 app.listen(port, ()=>{
-    console.log(`Servidor en funcionamiento en el puerto: `);
+    console.log(`Servidor en funcionamiento en el puerto : ${port}`);
 });
 
+/*
+http://localhost:3000
+*/
 
 

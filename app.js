d38f5bd43b3f9229a7982ce1fac03e32a4f419f2
JQ <julianq397@gmail.com>
2026-08-06 16:36:29 -0500

    Actividad 06/08

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const express = require('express');
+import 'dotenv/config'
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
@@ -7,6 +9,15 @@ app.get("/", (__,res)=>{
 });
 
 app.listen(port, ()=>{
-    console.log(`Servidor en funcionamiento en el puerto: `);
+    console.log(`Servidor en funcionamiento en el puerto: ${port} `);
 });
 
+app.get("/datos_personales", (req, res) => {
+    res.json({"Datos Perosnales":{"Nombre": "Samuel", "Apellido": "Bernal", "Lista de Telefonos": {
+            "telefono 1" : "3197929060",
+            "telefono 2": "3142940329"
+    }},
+    "Datos Programa":{
+        "nombre": "ADSO", "Tipo de programa": "Analisis y Desarrollo de Software", "ficha": "3407184"
+    }})
+});

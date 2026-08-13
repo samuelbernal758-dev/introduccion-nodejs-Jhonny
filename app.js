7fb931095a8f10b7694723f1f3f8856de15a1b83
JQ <julianq397@gmail.com>
2026-08-13 16:23:58 -0500

    Arreglos

--- app.js
+++ app.js
@@ -6,25 +6,39 @@ app.get("/", (__,res)=>{
     res.send ("API de Aprendices, EndPoint Principal");
 });
 
-app.get("/aprendices", (req, res) => {
-    res.json([{
-        "nombre": "Juan Camila",
+const ListaAprendices = [{
+        "nombre": "Juan",
         "Edad":  "17",
         "Correo": "juan@gmail.com",
         "imgPerfil": "url"
     },
     {
-        "nombre": "Derly Maday",
+        "nombre": "Derly",
         "Edad":  "19",
         "Correo": "Maday@gmail.com",
         "imgPerfil": "url"
     },
     {
-        "nombre": "Sebastian Gonzalez",
+        "nombre": "Sebastian",
         "Edad":  "17",
         "Correo": "sebas@gmail.com",
         "imgPerfil": "url"
-    }])
+    }]
+app.get("/aprendices", (req, res) =>{
+    res.json(ListaAprendices)
+})
+app.get('/aprendices/:nombre', (req, res) => {
+    const nombreBuscado = req.params.nombre.toLowerCase();
+
+    const aprendizencontrado = ListaAprendices.find(
+        (aprendiz) => aprendiz.nombre.toLowerCase() === nombreBuscado
+    );
+    res.json(aprendizencontrado);
+    
+});
+app.post("/aprendices", (req, res) =>{ 
+    const datosAprendiz = req.body
+    res.json({"Mensaje": "Aprendiz creado", "Datos": datosAprendiz})
 })
 
 app.listen(port, ()=>{
